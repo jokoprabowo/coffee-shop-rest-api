32971e5209091f4593e4fdb6d4c2822a06154762
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-04 22:09:11 +0700

    add service for update users data

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -3,9 +3,17 @@ const postgre = require('../database/index');
 const userService = {
     async create(createArgs){
         const { email, encrypt, name, address } = createArgs;
-        const sql = 'insert into users(email, password, name, address, created_at, updated_at)' +
+        const sql = 'insert into users(email, password, name, address, created_at, updated_at) ' +
                     'values($1, $2, $3, $4, $5, $6) returning *';
         const { rows } = await postgre.query(sql, [email, encrypt, name, address, new Date(), null]);
+        return rows[0];
+    },
+
+    async update(updateArgs){
+        const { email, encrypt, name, address } = updateArgs;
+        const sql = 'update users set password = $1, name = $2, address = $3, updated_at = $4 ' +
+                    'where email = $5 returning *';
+        const { rows } = await postgre.query(sql, [encrypt, name, address, new Date(), email]);
         return rows[0];
     },
 };
