2eed8620ab9236f9669fc2ba5707ae8cd52d5a0d
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-05 19:54:07 +0700

    add utility for encryption

---
+++ src/api/Utilities/encrypt.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt');
+const SALT = 10;
+
+module.exports = {
+    checkPassword(encryptedPassword, password){
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, encryptedPassword, (err, isPasswordCorrect) =>{
+                if(!err){
+                    reject(err);
+                    return;
+                };
+                resolve(isPasswordCorrect);
+            });
+        });
+    },
+
+    encryptPassword(password){
+        return new Promise((resolve, reject) => {
+            bcrypt.hash(password, SALT, (err, encryptedPassword) => {
+                if(!err){
+                    reject(err);
+                    return;
+                };
+                resolve(encryptedPassword);
+            });
+        });
+    },
+}
