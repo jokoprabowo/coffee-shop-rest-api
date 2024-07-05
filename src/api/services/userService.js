417a6868974120462c950c4b43b15275c0f0ecfe
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-06 01:03:22 +0700

    add service for register new account

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,27 +1,20 @@
-const postgre = require('../database/index');
+const userRepository = require('../repositories/userRepository');
+const { checkPassword, encryptPassword } = require('../Utilities/encrypt');
 
 const userService = {
-    async create(createArgs){
-        const { email, encrypt, name, address } = createArgs;
-        const sql = 'insert into users(email, password, name, address, created_at, updated_at) ' +
-                    'values($1, $2, $3, $4, $5, $6) returning *';
-        const { rows } = await postgre.query(sql, [email, encrypt, name, address, new Date(), null]);
-        return rows[0];
-    },
+    async register(args){
+        try{
+            const { email, password, name, address } = args;
+            const encrypt = encryptPassword(password);
+            const check = await userRepository.findOne(email);
+            if(check != 0){
+                throw new Error("Email already in use!");
+            }
+            const data = await userRepository.create({email, encrypt, name, address});
+            return data;
+        }catch(err){
+            throw err.message;
+        }
 
-    async findOne(email){
-        const sql = 'select * from users where email = $1';
-        const { rows } = await postgre.query(sql, [email]);
-        return rows;
-    },
-
-    async update(updateArgs){
-        const { email, encrypt, name, address } = updateArgs;
-        const sql = 'update users set password = $1, name = $2, address = $3, updated_at = $4 ' +
-                    'where email = $5 returning *';
-        const { rows } = await postgre.query(sql, [encrypt, name, address, new Date(), email]);
-        return rows[0];
-    },
-};
-
-module.exports = userService;
+    }
+}
