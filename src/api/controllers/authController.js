5877cffd6750937b4d6c9237c1d5cb0562f471bf
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-07 21:45:45 +0700

    add controller to login

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -1,32 +1,60 @@
 const userService = require('../services/userService');
-const { encryptPassword, checkPassword } = require('../utilities/encrypt');
-const { createToken } = require('../utilities/token');
-
 const authController = {
     async regis(req, res){
         try{
-            const { email, password, name, address } = req.body;
-            const encrypt = encryptPassword(password);
-            const check = await userService.findOne(email);
-            if(check != 0){
-                res.status(400).json({
-                    status: "FAIL",
-                    message: "Email already in use!",
-                });
-                return;
-            }
-            const data = await userService.create({email, encrypt, name, address});
-            delete data.password;
+            const user = await userService(req.body);
             res.status(201).json({
-                user: data,
-                status: "SUCCESSFULL",
+                status: "SUCCESS",
                 message: "Account successfully created!",
+                user: user
             });
         }catch(err){
-            res.status(500).json({
-                status: "INTERNAL ERROR",
-                message: err.message
-            })
+            if (err.message == "Email already in use!"){
+                res.status(400).json({
+                    status: "FAIL",
+                    message: err.message
+                });
+            }else{
+                res.status(500).json({
+                    status: "INTERNAL ERROR",
+                    message: err.message
+                });
+            }
+        }
+    },
+
+    async login(req, res){
+        try{
+            const token = await userService(req.body);
+            res.status(200).json({
+                status: "SUCCESS",
+                message: "Login successfull!",
+                token: token
+            });
+        }catch(err){
+            if(err.message == "It is not an email!"){
+                res.status(422).json({
+                    status: "FAIL",
+                    message: err.message
+                });
+            }else if(err.message == "User not found!"){
+                res.status(404).json({
+                    status: "FAIL",
+                    message: err.message
+                });
+            }else if(err.message == "Incorrect password!"){
+                res.status(400).json({
+                    status: "FAIL",
+                    message: err.message
+                });
+            }else{
+                res.status(500).json({
+                    status: "INTERNAL ERROR",
+                    message: err.message
+                });
+            }
         }
     }
 }
+
+module.exports = authController;
