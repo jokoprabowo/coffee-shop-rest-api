b145b637cf8d6d13aeeb6193c950aadcc1e4080a
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-09 21:38:26 +0700

    update functions

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -1,7 +1,18 @@
 const userService = require('../services/userService');
+const { validateEmail } = require('../utilities/email');
+const { checkPassword } = require('../utilities/encrypt');
+
 const authController = {
     async register(req, res){
         try{
+            const check = await userService.findOne(req.body.email);
+            if(check){
+                res.status(404).json({
+                    status: "FAIL",
+                    message: "Email already in used!",
+                });
+                return;
+            };
             const user = await userService.register(req.body);
             res.status(201).json({
                 status: "SUCCESS",
@@ -9,22 +20,39 @@ const authController = {
                 user: user
             });
         }catch(err){
-            if (err.message == "Email already in use!"){
-                res.status(400).json({
-                    status: "FAIL",
-                    message: err.message
-                });
-            }else{
-                res.status(500).json({
-                    status: "INTERNAL ERROR",
-                    message: err.message
-                });
-            }
+            res.status(500).json({
+                status: "INTERNAL ERROR",
+                message: err.message
+            });
         }
     },
 
     async login(req, res){
         try{
+            const isEmail = validateEmail(req.body.email);
+            if(!isEmail){
+                res.status(422).json({
+                    status: "FAIL",
+                    message: "It is not an email!"
+                });
+                return;
+            };
+            const user = await userService.findOne(req.body.email);
+            if(!user){
+                res.status(404).json({
+                    status: "FAIL",
+                    message: "Account not found!"
+                });
+                return;
+            }
+            const isMatch = checkPassword(user.password, req.body.password);
+            if(!isMatch){
+                res.status(400).json({
+                    status: "FAIL",
+                    message: "Incorrect password!"
+                });
+                return;
+            }
             const token = await userService.login(req.body);
             res.cookie("token", token, {
                 httpOnly: true,
@@ -33,27 +61,10 @@ const authController = {
                 message: "Login successfull!",
             });
         }catch(err){
-            if(err.message == "It is not an email!"){
-                res.status(422).json({
-                    status: "FAIL",
-                    message: err.message
-                });
-            }else if(err.message == "User not found!"){
-                res.status(404).json({
-                    status: "FAIL",
-                    message: err.message
-                });
-            }else if(err.message == "Incorrect password!"){
-                res.status(400).json({
-                    status: "FAIL",
-                    message: err.message
-                });
-            }else{
-                res.status(500).json({
-                    status: "INTERNAL ERROR",
-                    message: err.message
-                });
-            }
+            res.status(500).json({
+                status: "INTERNAL ERROR",
+                message: err.message
+            });
         }
     }
 }
--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,62 +1,38 @@
 const userRepository = require('../repositories/userRepository');
-const { checkPassword, encryptPassword } = require('../utilities/encrypt');
-const { createToken, verifyToken }  = require('../utilities/token');
-const { validateEmail } = require('../utilities/email');
+const { encryptPassword } = require('../utilities/encrypt');
+const { createToken }  = require('../utilities/token');
 
 const userService = {
     async register(args){
-        try{
-            const { email, password, name, address } = args;
-            const encrypt = encryptPassword(password);
-            const check = await userRepository.findOne(email);
-            if(check != 0){
-                throw new Error("Email already in use!");
-            }
-            const data = await userRepository.create({email, encrypt, name, address});
-            return data;
-        }catch(err){
-            throw err.message;
-        }
+        const { email, password, name, address } = args;
+        const encrypt = encryptPassword(password);
+        const data = await userRepository.create({email, encrypt, name, address});
+        return data;
     },
     
-    async login(args){
-        try{
-            const { email, password } = args;
-            const isEmail = validateEmail(email);
-            if(!isEmail){
-                throw new Error("It is not an email!");
-            }
-            const user = await userRepository.findOne(email);
-            if(user != 0){
-                throw new Error("User not found!");
-            };
-            const isMatch = checkPassword(user.password, password);
-            if(!isMatch){
-                throw new Error("Incorrect password!");
-            };
-            delete user.password;
-            const token = createToken(user);
-            return token;
-        }catch(err){
-            throw new Error(err.message);
-        }
+    async login(email){
+        const user = await userRepository.findOne(email);
+        delete user.password;
+        const token = createToken(user);
+        return token;
     },
 
     async update(args, auth){
-        try{
-            const { email, password, name, address } = args;
-            const encrypt = encryptPassword(password);
-            // const bearerToken = auth;
-            // const token = bearerToken.split("Bearer ")[1];
-            // const tokenPayload = verifyToken(token);
+        const { email, password, name, address } = args;
+        const encrypt = encryptPassword(password);
+        // const bearerToken = auth;
+        // const token = bearerToken.split("Bearer ")[1];
+        // const tokenPayload = verifyToken(token);
 
-            const data = await userRepository.update({email, encrypt, name, address}, auth);
-            delete data.password;
-            return data;
-        }catch(err){
-            throw new Error(err.message);
-        }
+        const data = await userRepository.update({email, encrypt, name, address}, auth);
+        delete data.password;
+        return data;
     },
+
+    async findOne(email){
+        const user = await userRepository.findOne(email);
+        return user;
+    }
 }
 
 module.exports = userService;
