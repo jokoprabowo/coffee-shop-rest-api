2eece5d9c812ad806a54a9833b3eb137d24cc864
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-12 21:31:37 +0700

    add repository function to create cookie data

---
+++ src/api/repositories/cookieRepository.js
@@ -0,0 +1,13 @@
+const postgre = require('../database/index');
+
+const cookieRepository = {
+    async create(createArgs){
+        const { name, description, price, image } = createArgs;
+        const sql = 'insert into cookies(name, description, price, image, created_at, updated_at) ' +
+                    'values($1, $2, $3, $4, $5, $6) returning *';
+        const { rows } = await postgre.query(sql, [name, description, price, image, new Date(), null]);
+        return rows[0];
+    }
+}
+
+module.exports = cookieRepository;
