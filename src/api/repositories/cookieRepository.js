90510421c5ed0509e24d71f08902f8983491e1e0
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-12 21:37:38 +0700

    add repository function to retrieve all cookies data

--- src/api/repositories/cookieRepository.js
+++ src/api/repositories/cookieRepository.js
@@ -7,6 +7,12 @@ const cookieRepository = {
                     'values($1, $2, $3, $4, $5, $6) returning *';
         const { rows } = await postgre.query(sql, [name, description, price, image, new Date(), null]);
         return rows[0];
+    },
+
+    async findAll(){
+        const sql = 'select * from cookies';
+        const { rows } = await postgre.query(sql);
+        return rows;
     }
 }
 
