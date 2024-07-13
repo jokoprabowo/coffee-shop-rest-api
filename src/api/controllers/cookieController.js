72bab292cbea17bfe5cbfa5a2419507219c6df51
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-13 20:29:20 +0700

    add controller for get single cookie data

--- src/api/controllers/cookieController.js
+++ src/api/controllers/cookieController.js
@@ -6,20 +6,37 @@ const cookieController = {
             const data = await cookieService.create(req.body);
             res.status(201).json({
                 status: "SUCCESS",
-                message: "Cookie has been added!"
+                message: "Cookie has been added!",
+                data: data
             });
         }catch(err){
             if(err.message == "Cookie already exist!"){
                 res.status(400).json({
                     status: "FAIL",
                     message: err.message
-                })
+                });
             }else{
                 res.status(500).json({
                     status: "INTERNAL ERROR",
                     message: err.message
-                })
+                });
             }
+        }
+    },
+
+    async getOne(req, res){
+        try{
+            const data = await cookieService.findOne(req.body.name);
+            res.status(201).json({
+                status: "SUCCESS",
+                message: "Cookie has been retrieved!",
+                data: data
+            });
+        }catch(err){
+            res.status(500).json({
+                status: "INTERNAL ERROR",
+                message: err.message
+            });
         }
     }
 };
