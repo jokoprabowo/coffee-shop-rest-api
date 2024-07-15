7c300fb0f5ddefda3d8baceb804965934a507c95
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-15 20:49:31 +0700

    serve API for cookies

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 const userRouter = require('./src/api/routers/userRoute');
+const cookieRoute = require('./src/api/routers/cookieRoute');
 
 app.use('/api/user', userRouter);
+app.use('/api/cookie', cookieRoute);
 
 app.listen(process.env.PORT, () => console.log(`Server is running on port: http://localhost:${process.env.PORT}`));
