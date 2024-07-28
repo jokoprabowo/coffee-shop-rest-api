37a372da686a083dba4a15b75ee8845540329dbb
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-28 21:05:26 +0700

    add more fields

--- src/api/repositories/transactionRepository.js
+++ src/api/repositories/transactionRepository.js
@@ -1,7 +1,13 @@
 const postgre = require('../database/index');
 
 const transactionRepository = {
-
+    async create(args){
+        const { userId, cookieId, totalItems, totalPrice } = args;
+        const sql = 'insert into transactions(userId, cookieId, totalItem, totalPrice, created_at, updated_at) ' +
+                    'values($1, S2, $3, $4, $5, $6) returning *';
+        const data = postgre.query(sql, [userId, cookieId, totalItems, totalPrice, new Date(), null]);
+        return data[0];
+    },
 };
 
 module.exports = transactionRepository;
