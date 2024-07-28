ab2ba7d06fb4ef4006807c0ed80e8ef1788c8c71
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-28 21:13:13 +0700

    add function to create transaction data

--- src/api/repositories/transactionRepository.js
+++ src/api/repositories/transactionRepository.js
@@ -5,8 +5,8 @@ const transactionRepository = {
         const { userId, cookieId, totalItems, totalPrice } = args;
         const sql = 'insert into transactions(userId, cookieId, totalItem, totalPrice, created_at, updated_at) ' +
                     'values($1, S2, $3, $4, $5, $6) returning *';
-        const data = postgre.query(sql, [userId, cookieId, totalItems, totalPrice, new Date(), null]);
-        return data[0];
+        const { rows } = await postgre.query(sql, [userId, cookieId, totalItems, totalPrice, new Date(), null]);
+        return rows[0];
     },
 };
 
