dc8cd4e1dff5255c5983ecc5b3f0b434b6e1654e
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-28 21:13:48 +0700

    add function to retrieve transaction data

--- src/api/repositories/transactionRepository.js
+++ src/api/repositories/transactionRepository.js
@@ -8,6 +8,18 @@ const transactionRepository = {
         const { rows } = await postgre.query(sql, [userId, cookieId, totalItems, totalPrice, new Date(), null]);
         return rows[0];
     },
+
+    async findAll(){
+        const sql = 'select * from transactions';
+        const { rows } = await postgre.query(sql);
+        return rows;
+    },
+
+    async findOne(id){
+        const sql = 'select * from transactions where id = $1';
+        const { rows } = await postgre.query(sql, [id]);
+        return rows[0];
+    }
 };
 
 module.exports = transactionRepository;
