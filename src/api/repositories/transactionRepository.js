dce22ff7ce3bd0c16be22baf629b4f2eb89eb31b
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-07-29 20:52:52 +0700

    add function to update transaction data

--- src/api/repositories/transactionRepository.js
+++ src/api/repositories/transactionRepository.js
@@ -19,6 +19,14 @@ const transactionRepository = {
         const sql = 'select * from transactions where id = $1';
         const { rows } = await postgre.query(sql, [id]);
         return rows[0];
+    },
+
+    async update(args, id){
+        const { userId, cookieId, totalItems, totalPrice } = args;
+        const sql = 'update set userId = $1, cookieId = $2, totalItems = $3, totalPrice = $4, update_at = $5 ' +
+                    'where id = $6 returning *';
+        const { rows } = await postgre.query(sql, [userId, cookieId, totalItems, totalPrice, new Date()]);
+        return rows[0];
     }
 };
 
