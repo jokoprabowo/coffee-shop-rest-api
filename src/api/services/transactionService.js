5e53f1abc12b05fe1d0b855fdaa4d9c227147eb6
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-08-05 20:28:01 +0700

    add function to create transaction data

--- src/api/services/transactionService.js
+++ src/api/services/transactionService.js
@@ -1,7 +1,14 @@
 const transactionRepository = require('../repositories/transactionRepository');
 
 const transactionService = {
-
+    async create(args){
+        try{
+            const data = await transactionRepository.create(args);
+            return data;
+        }catch(err){
+            throw new Error(err.message);
+        }
+    },
 };
 
 module.exports = transactionService;
