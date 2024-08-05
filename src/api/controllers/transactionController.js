f460eea9006d6a07ef55eed081e5934111c201b7
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-08-05 21:33:31 +0700

    add create function for transaction data

--- src/api/controllers/transactionController.js
+++ src/api/controllers/transactionController.js
@@ -1,7 +1,17 @@
 const transactionService = require('../services/transactionService');
 
 const transactionController = {
-
+    async create(args){
+        try{
+            const data = await transactionService.create(args);
+            return data;
+        }catch(err){
+            res.status.json({
+                status: "INTERNAL ERROR",
+                message: err.message
+            })
+        }
+    }
 };
 
 module.exports = transactionController;
