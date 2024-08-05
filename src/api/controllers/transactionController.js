81534038b3c51e8bb3063d3574af51344518f170
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-08-05 21:45:05 +0700

    add read function for transaction data

--- src/api/controllers/transactionController.js
+++ src/api/controllers/transactionController.js
@@ -1,15 +1,58 @@
 const transactionService = require('../services/transactionService');
 
 const transactionController = {
-    async create(args){
+    async create(req, res){
         try{
-            const data = await transactionService.create(args);
-            return data;
+            const data = await transactionService.create(req.body);
+            res.status(201).json({
+                status: "SUCCESS",
+                message: "Transaction success!",
+                data
+            })
         }catch(err){
-            res.status.json({
+            res.status(500).json({
                 status: "INTERNAL ERROR",
                 message: err.message
             })
+        }
+    },
+
+    async getAll(req, res){
+        try{
+            const data = await transactionService.findAll();
+            res.status(200).json({
+                status: "SUCCESS",
+                message: "Transactions data have been retrieved!",
+                data,
+            })
+        }catch(err){
+            res.status(500).json({
+                status: "INTERNAL ERROR",
+                message: err.message
+            })
+        }
+    },
+
+    async getOne(req, res){
+        try{
+            const data = await transactionService.getOne(req.params.id);
+            res.status(201).json({
+                status: "SUCCESS",
+                message: "Transaction has been retrieved!",
+                data
+            })
+        }catch(err){
+            if(err.message == "Transaction not found!"){
+                res.status(404). json({
+                    status: "FAIL",
+                    message: err.message
+                });
+            }else{
+                res.status(500).json({
+                    status: "INTERNAL ERROR",
+                    message: err.message
+                });
+            }
         }
     }
 };
