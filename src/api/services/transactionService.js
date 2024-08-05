b2c2ef05ecb5a60591e13c6a1b9455af534328a6
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-08-05 20:33:46 +0700

    add function to retrieve transaction data

--- src/api/services/transactionService.js
+++ src/api/services/transactionService.js
@@ -9,6 +9,27 @@ const transactionService = {
             throw new Error(err.message);
         }
     },
+
+    async findOne(id){
+        try{
+            const data = await transactionRepository.findOne(id);
+            if(!data){
+                throw new Error("Transaction not found!");
+            }
+            return data;
+        }catch(err){
+            throw new Error(err.message);
+        }
+    },
+
+    async findAll(){
+        try{
+            const data = await transactionRepository.findAll();
+            return data;
+        }catch(err){
+            throw new Error(err.message);
+        }
+    }
 };
 
 module.exports = transactionService;
