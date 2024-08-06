9657d281cc28c193416f6ad9f3bf1d9ac7f261c7
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-08-06 20:28:25 +0700

    create router for transaction

---
+++ src/api/routers/transactionRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const transactionController = require('../controllers/transactionController');
+
+router.get("", transactionController.getAll);
+router.get("/:id", transactionController.getOne);
+router.post("", transactionController.create);
+router.put("/:id", transactionController.update);
