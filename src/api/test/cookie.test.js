f8d41079e0233cfd3fff412618a72d60f42eb9c7
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-08-31 20:50:33 +0700

    add test for update cookie endpoint

--- src/api/test/cookie.test.js
+++ src/api/test/cookie.test.js
@@ -63,3 +63,31 @@ describe("GET /api/cookie", () => {
     });
 
 });
+
+describe("PUT /api/cookie/:id", () => {
+
+    describe("Given all requirement data with correct cookie id", () => {
+        test("Should respond with a 201 status code", async () => {
+            const response = await request(app).put("/api/cookie/:1").send({
+                name: "Chocolate Chip",
+                description: "Classic sensation with rich semi-sweet chocolate in soft buttery dough",
+                price: 20,
+                image: null             
+             });
+             expect(response.statusCode).toBe(201);
+        });
+    });
+
+    describe("Given all requirement data with wrong cookie id", () => {
+        test("Should respond with a 404 status code", async () => {
+            const response = await request(app).put("/api/cookie/:10").send({
+                name: "Chocolate Chip",
+                description: "Classic sensation with rich semi-sweet chocolate in soft buttery dough",
+                price: 20,
+                image: null             
+             });
+             expect(response.statusCode).toBe(404);
+        });
+    });
+
+});
