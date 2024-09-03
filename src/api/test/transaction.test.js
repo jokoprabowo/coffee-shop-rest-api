00912ccf8b790de5db76085b7f2279f65a0caf52
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-09-03 22:05:03 +0700

    commented endpoint for create transaction

--- src/api/test/transaction.test.js
+++ src/api/test/transaction.test.js
@@ -3,25 +3,25 @@ const app = require('../app');
 
 describe("POST /api/transaction", () => {
 
-    describe("Given all requirements data", () => {
-        test("Should respond with a 201 status code", async () => {
-            const data = await request(app).get("/api/user/login").send({
-                email: "test22@gmail.com",
-                password: "test1234",
-                name: "test",
-                address: "test"
-            });
+    // describe("Given all requirements data", () => {
+    //     test("Should respond with a 201 status code", async () => {
+    //         const data = await request(app).get("/api/user/login").send({
+    //             email: "test22@gmail.com",
+    //             password: "test1234",
+    //             name: "test",
+    //             address: "test"
+    //         });
         
-            const obj = data.headers['set-cookie'];
-            const cookie = obj[0].slice(0, -18);
+    //         const obj = data.headers['set-cookie'];
+    //         const cookie = obj[0].slice(0, -18);
             
-            const response = await request(app).post("/api/transaction").set("Cookie", cookie).send({
-                cookieId : 1,
-                totalItem : 2
-            });
-            expect(response.statusCode).toBe(201);
-        });
-    });
+    //         const response = await request(app).post("/api/transaction").set("Cookie", cookie).send({
+    //             cookieId : 1,
+    //             totalItem : 2
+    //         });
+    //         expect(response.statusCode).toBe(201);
+    //     });
+    // });
 
 });
 
