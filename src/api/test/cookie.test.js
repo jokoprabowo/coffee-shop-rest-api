4a42566e0c27e6d0ce079cc34392d18c657515f5
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-09-03 22:08:31 +0700

    commented endpoint for create cookie

--- src/api/test/cookie.test.js
+++ src/api/test/cookie.test.js
@@ -3,17 +3,17 @@ const app = require('../app');
 
 describe("POST /api/cookie", () => {
 
-    describe("Given an unused cookie name", () => {
-        test("Should respond with a 201 status code", async () => {
-            const response = await request(app).post("/api/cookie").send({
-                name: "Chocolate Chip",
-                description: "Classic sensation with rich semi-sweet chocolate in soft buttery dough",
-                price: 20,
-                image: null
-            });
-            expect(response.statusCode).toBe(201);
-        });
-    });
+    // describe("Given an unused cookie name", () => {
+    //     test("Should respond with a 201 status code", async () => {
+    //         const response = await request(app).post("/api/cookie").send({
+    //             name: "Chocolate Chip",
+    //             description: "Classic sensation with rich semi-sweet chocolate in soft buttery dough",
+    //             price: 20,
+    //             image: null
+    //         });
+    //         expect(response.statusCode).toBe(201);
+    //     });
+    // });
 
     describe("Given an used cookie name", () => {
         test("Should respond with a 400 status code", async () => {
