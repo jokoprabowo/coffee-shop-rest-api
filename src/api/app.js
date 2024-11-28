b4e5267867c0597bfd291715c78e340df746a3fa
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-11-28 23:49:42 +0700

    set views and static files

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const { logger } = require('./middlewares/logger');
 const corsOptions = require('./config/corsOptions');
@@ -9,18 +10,27 @@ const app = express();
 require('dotenv').config();
 const swaggerDoc = require('../docs/swagger.json');
 
+app.set('views', path.join(__dirname, '../public/views'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'ejs');
 app.use(cors(corsOptions));
 app.use(logger);
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(express.static('public'));
 
 const userRoute = require('./routers/userRoute');
 const cookieRoute = require('./routers/cookieRoute');
 const transactionRoute = require('./routers/transactionRoute');
 
+app.get('', (req, res) => {
+  res.render('index');
+})
+
 app.use('/api/user', userRoute);
 app.use('/api/cookie', cookieRoute);
 app.use('/api/transaction', transactionRoute);
-app.use(errorHandler);
+// app.use(errorHandler);
 
 module.exports = app;
