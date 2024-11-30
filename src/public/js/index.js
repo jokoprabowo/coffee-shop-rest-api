09055bdf90a9470fafdc8557d2d1f19a9df3dde6
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-11-30 21:13:33 +0700

    add map layers

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([-3.513, 121.733], 5);
 
-let openStreet = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
@@ -14,13 +14,11 @@ let googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
   subdomains:['mt0','mt1','mt2','mt3']
 });
 
-let mapList = [ openStreet, googleStreets, googleSat ];
+osm.addTo(map);
+googleStreets.addTo(map);
+googleSat.addTo(map);
 
-mapList.map(item => {
-  item.addTo(map);
-});
-
-var theMarker = {};
+let theMarker = {};
 
   map.on('click',function(e){
     lat = e.latlng.lat;
@@ -36,3 +34,11 @@ var theMarker = {};
     //Add a marker to show where you clicked.
     theMarker = L.marker([lat,lon]).addTo(map);
 });
+
+let baseLayers = {
+  'OpenStreetMap': osm,
+  'Google Street': googleStreets,
+  'Satelite': googleSat,
+};
+
+L.control.layers(baseLayers).addTo(map);
