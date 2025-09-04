c62deb46c82b65c8438cf8093c6ffb31860cba80
Joko Prabowo <jokoprabowo4550@gmail.com>
2025-09-04 21:55:37 +0700

    feat(cart): add new endpoints for cart and cart item

---
+++ migrations/1756820804227_create-carts-table.ts
@@ -0,0 +1,25 @@
+import { MigrationBuilder } from 'node-pg-migrate';
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable('carts', {
+    id: 'id',
+    user_id: {
+      type: 'integer',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+
+  pgm.createIndex('carts', 'user_id');
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropIndex('carts', 'user_id');
+  pgm.dropTable('carts');
+}
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,9 +1,11 @@
 import CoffeeRepository from './coffee.repository';
 import UserRepository from './user.repository';
 import RefreshTokenRepository from './refreshtoken.repository';
+import CartRepository from './cart.repository';
 
 export {
   CoffeeRepository,
   UserRepository,
   RefreshTokenRepository,
+  CartRepository,
 };
