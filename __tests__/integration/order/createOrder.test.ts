b4b31b95da9a277901df7d5f657b33021213e09d
Joko Prabowo <jokoprabowo4550@gmail.com>
2025-09-21 23:42:38 +0700

    test(order): create integration tests for all order endpoints

---
+++ __tests__/integration/order/createOrder.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import app from '../../../src/app';
+import pool from '../../../src/config/db';
+
+describe('Create order endpoint.', () => {
+  let userId: number;
+  let token: string;
+
+  beforeAll(async () => {
+    const res = await request(app).post('/api/v1/auth/register')
+      .send({
+        email: 'testexample@gmail.com',
+        password: 'Example!test123',
+        fullname: 'Test Example',
+        phone: '081234567890',
+        address: 'Test street, Example, 00000'
+      });
+
+    token = res.body.data.accessToken;
+    userId = res.body.data.user.id;
+  });
+
+  afterAll(async () => {
+    await pool.query('delete from users where id = $1', [userId]);
+    await pool.end();
+  });
+
+  it('Should return a 201 status code if order successfully created.', async () => {
+    await request(app).post('/api/v1/cart')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ coffeeId: 1, quantity: 1, });
+
+    const response = await request(app).post('/api/v1/order')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.status).toBe('CREATED');
+    expect(response.body.data).toHaveProperty('order');
+  });
+
+  it('Should return a 401 status code if access token is missing.', async () => {
+    const response = await request(app).post('/api/v1/order');
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.status).toBe('UNAUTHENTICATED');
+  });
+
+  it('Should return a 404 status code if the cart with provided id is not exist.', async () => {
+    const response = await request(app).post('/api/v1/order')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.status).toBe('NOT_FOUND');
+
+  });
+});
---
+++ __tests__/integration/order/deleteOrder.test.ts
@@ -0,0 +1,60 @@
+import request from 'supertest';
+import app from '../../../src/app';
+import pool from '../../../src/config/db';
+
+describe('Delete order endpoint.', () => {
+  let userId: number;
+  let token: string;
+  let orderId: number;
+
+  beforeAll(async () => {
+    const res = await request(app).post('/api/v1/auth/register')
+      .send({
+        email: 'testexample@gmail.com',
+        password: 'Example!test123',
+        fullname: 'Test Example',
+        phone: '081234567890',
+        address: 'Test street, Example, 00000'
+      });
+
+    token = res.body.data.accessToken;
+    userId = res.body.data.user.id;
+
+    await request(app).post('/api/v1/cart')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ coffeeId: 1, quantity: 1, });
+
+    const order = await request(app).post('/api/v1/order')
+      .set('Authorization', `Bearer ${token}`);
+
+    orderId = order.body.data.order.id;
+  });
+
+  afterAll(async () => {
+    await pool.query('delete from users where id = $1', [userId]);
+    await pool.end();
+  });
+
+  it('Should return a 200 status code if order successfully deleted.', async () => {
+    const response = await request(app).delete(`/api/v1/order/${orderId}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe('OK');
+  });
+
+  it('Should return a 401 status code if access token is missing.', async () => {
+    const response = await request(app).delete(`/api/v1/order/${orderId}`);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.status).toBe('UNAUTHENTICATED');
+  });
+
+  it('Should return a 404 status code if the cart with provided id is not exist.', async () => {
+    const response = await request(app).delete('/api/v1/order/-1')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.status).toBe('NOT_FOUND');
+  });
+});
