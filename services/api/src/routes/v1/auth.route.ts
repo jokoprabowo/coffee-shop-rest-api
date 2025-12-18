9184dfd54a401bb45d84011b552e3db8e6bb5789
Joko Prabowo <jokoprabowo4550@gmail.com>
2025-12-19 06:54:57 +0700

    feat: add verification endpoints

--- services/api/src/routes/v1/auth.route.ts
+++ services/api/src/routes/v1/auth.route.ts
@@ -1,19 +1,26 @@
 import { Router } from 'express';
 import { pool } from '@project/shared';
 import { AuthController } from '../../controllers';
-import { AuthService, UserService, RefreshTokenService } from '../../services';
-import { UserRepository, RefreshTokenRepository } from '../../repositories';
+import { AuthService, UserService, RefreshTokenService, ProducerService } from '../../services';
+import { UserRepository, RefreshTokenRepository, VerificationRepository } from '../../repositories';
 import AuthenticateValidator from '../../validators/authentication';
 
-const repository = new UserRepository(pool);
-const service = new AuthService(new UserService(repository));
-const controller = new AuthController(service, new RefreshTokenService(new RefreshTokenRepository(pool)), AuthenticateValidator);
+const userRepository = new UserRepository(pool);
+const verificationRepository = new VerificationRepository(pool);
+const refreshTokenRepository = new RefreshTokenRepository(pool);
+
+const userService = new UserService(userRepository);
+const authService = new AuthService(userService, verificationRepository, ProducerService);
+const refreshTokenService = new RefreshTokenService(refreshTokenRepository);
+
+const authController = new AuthController(authService, refreshTokenService, AuthenticateValidator);
 const router = Router();
 
-router.post('/login', controller.login);
-router.post('/register', controller.register);
-router.post('/refresh-token', controller.refreshToken);
-router.delete('/logout', controller.logout);
+router.post('/login', authController.login);
+router.post('/register', authController.register);
+router.post('/verify', authController.verifyEmail);
+router.post('/refresh-token', authController.refreshToken);
+router.delete('/logout', authController.logout);
 
 
 export default router;
