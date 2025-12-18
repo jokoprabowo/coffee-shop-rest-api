3a6c896f5ed6b86bced4444f2c226cf1e5ff143a
Joko Prabowo <jokoprabowo4550@gmail.com>
2025-12-19 06:45:03 +0700

    feat: add verification repository

--- services/api/src/repositories/index.ts
+++ services/api/src/repositories/index.ts
@@ -3,3 +3,4 @@ export { default as UserRepository } from './user.repository';
 export { default as RefreshTokenRepository } from './refreshtoken.repository';
 export { default as CartRepository } from './cart.repository';
 export { default as OrderRepository } from './order.repository';
+export { default as VerificationRepository } from './verification.repository';
