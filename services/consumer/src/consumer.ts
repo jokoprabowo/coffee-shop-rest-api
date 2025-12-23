7f76ea28a7ed0a42327427d2e9cb397fab1c04fd
Joko Prabowo <jokoprabowo4550@gmail.com>
2025-12-23 20:44:56 +0700

    feat: add email verification listener to consumer

--- services/consumer/src/consumer.ts
+++ services/consumer/src/consumer.ts
@@ -1,21 +1,40 @@
-import { CartRepository } from './repositories';
-import { CartService } from './services';
+import { UserRepository, CartRepository } from './repositories';
+import { UserService, CartService, EmailService } from './services';
 import { pool, logger, connectRabbitMQ } from '@project/shared';
+import { mailer } from './config';
 import Listener from './listener';
 
 const init = async (): Promise<void> => {
   try {
+    const userService = new UserService(new UserRepository(pool));
     const cartService = new CartService(new CartRepository(pool));
+    const emailService = new EmailService(mailer);
     const channel = await connectRabbitMQ();
+    const listener = new Listener(userService, cartService, emailService, channel);
 
-    await channel.assertQueue('checkout', {
-      durable: true,
-    });
+    const queues = ['checkout', 'email_verification'];
 
-    const listener = new Listener(cartService, channel);
+    for (const queue of queues) {
+      await channel.assertQueue(queue, {
+        durable: true,
+      });
 
-    logger.info('Listening to queue: checkout');
-    channel.consume('checkout', listener.listen, { noAck: false });
+      channel.consume(queue, (msg) => {
+        switch (queue) {
+          case 'checkout':
+            return listener.listenOrder(msg);
+          
+          case 'email_verification':
+            return listener.listenEmail(msg);
+          
+          default:
+            logger.warn('No handler for queue:', queue);
+        }
+      }, { noAck: false });
+
+      logger.info(`Listening to queue: ${queue}`);
+    }
+
   } catch (err) {
     logger.error('Error initializing consumer service', err);
   }
