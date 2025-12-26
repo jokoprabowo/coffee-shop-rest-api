cd04eaea9fd69314414de08cc272acf8af7b341f
Joko Prabowo <jokoprabowo4550@gmail.com>
2025-12-26 21:48:04 +0700

    feat: add a function to send password reset email

--- services/consumer/src/services/email.service.ts
+++ services/consumer/src/services/email.service.ts
@@ -1,5 +1,5 @@
 import config, { mailer } from '../config';
-import { verificationEmailTemplate } from '../templates';
+import { verificationEmailTemplate, passwordResetEmailTemplate } from '../templates';
 
 class EmailService {
   private readonly mailer: typeof mailer;
@@ -8,15 +8,29 @@ class EmailService {
     this.mailer = emailClient;
   }
 
-  public async sendVerificationEmail(to: string, token: string): Promise<void> {
+  public async sendVerificationEmail(to: string, fullname: string, token: string, expiresAt: string): Promise<void> {
     const verificationLink = `http://localhost:3000/verify?token=${token}`;
-    const verificationTemplate = verificationEmailTemplate(verificationLink);
+    const verificationTemplate = verificationEmailTemplate('Coffee Shop', fullname, verificationLink, expiresAt);
 
     const mailOptions = {
       from: `"Coffee Shop" <${config.SMTP_USER}>`,
       to,
       subject: 'Email Verification',
       html: verificationTemplate,
+    };
+
+    await this.mailer.sendMail(mailOptions);
+  }
+
+  public async sendResetPassword(to: string, fullname: string, token: string, expiresAt: string): Promise<void> {
+    const resetPasswordLink = `http://localhost:3000/reset-password?token=${token}`;
+    const resetPasswordTemplate = passwordResetEmailTemplate('Coffee Shop', fullname, resetPasswordLink, expiresAt);
+
+    const mailOptions = {
+      from: `"Coffee Shop" <${config.SMTP_USER}>`,
+      to,
+      subject: 'Reset Password',
+      html: resetPasswordTemplate,
     };
 
     await this.mailer.sendMail(mailOptions);
