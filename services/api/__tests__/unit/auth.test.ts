b0daca256c3d26bb8074c86970c23157c38766c1
Joko Prabowo <jokoprabowo4550@gmail.com>
2025-12-29 21:37:06 +0700

    test: add unit test for verify and reset password

--- services/api/__tests__/unit/auth.test.ts
+++ services/api/__tests__/unit/auth.test.ts
@@ -1,14 +1,19 @@
-import { AuthService, UserService } from '../../src/services';
+import { AuthService, UserService, ProducerService } from '../../src/services';
+import { UserTokenRepository } from '../../src/repositories';
 import { checkInput } from '../../src/utilities/encrypt';
 import { ClientError } from '../../src/exceptions';
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
 jest.mock('bcrypt');
 jest.mock('../../src/utilities/encrypt', () => ({
   checkInput: jest.fn(),
 }));
+jest.mock('crypto');
 
 describe('Auth Service', () => {
+  let userTokenRepository: jest.Mocked<UserTokenRepository>;
+  let producerService: jest.Mocked<typeof ProducerService>;
   let userService: jest.Mocked<UserService>;
   let authService: AuthService;
 
@@ -17,15 +22,37 @@ describe('Auth Service', () => {
     phone: '081234567890', address: 'Test street, Example, 00000'
   };
 
+  const mockUserToken = {
+    user_id: 1,
+    token: 'hashedTokenValue',
+    type: 'PASSWORD_RESET',
+    expires_at: expect.any(String),
+  };
+
   beforeEach(() => {
     userService = {
       create: jest.fn(),
       findOne: jest.fn(),
     } as unknown as jest.Mocked<UserService>;
 
-    authService = new AuthService(userService);
+    userTokenRepository = {
+      create: jest.fn(),
+      findByToken: jest.fn(),
+      updateUsedAt: jest.fn(),
+      deleteByUserId: jest.fn(),
+    } as unknown as jest.Mocked<UserTokenRepository>;
+
+    producerService = {
+      sendMessage: jest.fn(),
+    } as unknown as jest.Mocked<typeof ProducerService>;
+
+    authService = new AuthService(userService, userTokenRepository, producerService);
     (bcrypt.hash as jest.Mock).mockResolvedValue('hashedPass');
     (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+    jest
+      .spyOn(authService, 'verifyToken')
+      .mockResolvedValue({ user_id: 1, token: 'sampleToken', type: 'PASSWORD_RESET', expires_at: new Date().toISOString() });
   });
 
   afterEach(() => {
@@ -68,6 +95,99 @@ describe('Auth Service', () => {
       await expect(
         authService.login({ email: 'test@mail.com', password: 'falsePassword!123' })
       ).rejects.toThrow(new ClientError('Password invalid!'));
+    });
+  });
+
+  describe('Find By Email', () => {
+    it('Should return user if email exists', async () => {
+      userService.findOne.mockResolvedValue(mockUser);
+
+      const result = await authService.findByEmail(mockUser.email);
+
+      expect(userService.findOne).toHaveBeenCalledWith(mockUser.email);
+      expect(result).toBe(mockUser);
+    });
+  });
+
+  describe('Update Used At', () => {
+    it('Should return true if update used at token successfully', async () => {
+      userTokenRepository.updateUsedAt.mockResolvedValue(true);
+
+      const result = await authService.updateUsedAt('sampleToken');
+
+      expect(userTokenRepository.updateUsedAt).toHaveBeenCalledWith('sampleToken');
+      expect(result).toBe(true);
+    });
+
+    it('Should return false if update used at token fails', async () => {
+      userTokenRepository.updateUsedAt.mockResolvedValue(false);
+
+      const result = await authService.updateUsedAt('sampleToken');
+
+      expect(userTokenRepository.updateUsedAt).toHaveBeenCalledWith('sampleToken');
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('Create User Token', () => {
+    it('Should create user token and send message successfully', async () => {
+      userService.findById.mockResolvedValue(mockUser);
+      
+      (crypto.randomBytes as jest.Mock).mockReturnValue({ toString: jest.fn().mockReturnValue('randomTokenValue') });
+      (crypto.randomBytes as jest.Mock).mockReturnValue({
+        update: jest.fn().mockReturnThis(), digest: jest.fn().mockReturnValue('hashedTokenValue') });
+
+      const result = await authService.createUserToken(1, 'PASSWORD_RESET');
+
+      expect(result).toBe('randomTokenValue');
+      expect(userTokenRepository.create).toHaveBeenCalledTimes(1);
+      expect(userTokenRepository.create).toHaveBeenCalledWith(
+        expect.objectContaining(mockUserToken)
+      );
+      expect(userService.findById).toHaveBeenCalledWith(1);
+      expect(producerService.sendMessage).toHaveBeenCalledWith('PASSWORD_RESET',
+        expect.objectContaining({
+          email: mockUser.email,
+          fullname: mockUser.fullname,
+          token: 'randomTokenValue',
+          expires_at: expect.any(String),
+        })
+      );
+    });
+  });
+
+  describe('Reset Password', () => {
+    it('Should reset password successfully', async () => {
+      await authService.resetPassword('sampleToken', 'newPassword!123');
+
+      expect(authService.verifyToken).toHaveBeenCalledWith('sampleToken');
+      expect(userService.update).toHaveBeenCalledWith(expect.any(Number), { password: 'newPassword!123' });
+    });
+  });
+
+  describe('Verify Token', () => {
+    it('Should return user token data if token is valid', async () => {
+      (crypto.createHash as jest.Mock).mockReturnValue({
+        update: jest.fn().mockReturnThis(), digest: jest.fn().mockReturnValue('hashedSampleToken')
+      });
+
+      userTokenRepository.findByToken.mockResolvedValue(mockUserToken);
+
+      const result = await authService.verifyToken('sampleToken');
+
+      expect(userTokenRepository.findByToken).toHaveBeenCalledWith('hashedSampleToken');
+      expect(result).toBe(mockUserToken);
+    });
+
+    it('Should throw ClientError if token is invalid', async () => {
+      (crypto.createHash as jest.Mock).mockReturnValue({
+        update: jest.fn().mockReturnThis(), digest: jest.fn().mockReturnValue('hashedSampleToken')
+      });
+
+      userTokenRepository.findByToken.mockResolvedValue(null);
+      await expect(
+        authService.verifyToken('sampleToken')
+      ).rejects.toThrow(new ClientError('Invalid or expired token'));
     });
   });
 });
