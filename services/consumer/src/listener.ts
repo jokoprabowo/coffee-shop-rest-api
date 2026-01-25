75b8b8a227d644ea641155dbf9db8f90a0032000
Joko Prabowo <jokoprabowo4550@gmail.com>
2026-01-25 20:41:18 +0700

    feat: apply transaction to listen order flow

--- services/consumer/src/listener.ts
+++ services/consumer/src/listener.ts
@@ -1,10 +1,11 @@
 import { CartService, EmailService, UserService } from './services';
 import { Channel, ConsumeMessage } from 'amqplib';
-import { logger } from '@project/shared';
+import { logger, Database } from '@project/shared';
 import { CartItemDTO } from './dto/cart.dto';
 
 class Listener{
   constructor(
+    private readonly db: Database,
     private readonly userService: UserService,
     private readonly cartService: CartService,
     private readonly emailService: EmailService,
@@ -17,23 +18,25 @@ class Listener{
 
   public async listenOrder(message: ConsumeMessage | null): Promise<void> {
     return this.structure(message, async (msg) => {
-      const { userId, cartItems } = JSON.parse(msg.content.toString());
-      const isUserExist = await this.userService.isUserExist(userId);
-      if (!isUserExist) {
-        return;
-      }
-
-      let cart = await this.cartService.isCartExist(userId);
-      cart ??= await this.cartService.createCart(userId);
-
-      cartItems.find( async (item: CartItemDTO) => {
-        if (typeof item.cart_item_id === 'string') {
-          await this.cartService.addToCart(cart.id, item.coffee_id, item.quantity);
+      return this.db.withTransaction(async () => {
+        const { userId, cartItems } = JSON.parse(msg.content.toString());
+        const isUserExist = await this.userService.isUserExist(userId);
+        if (!isUserExist) {
+          return;
         }
+
+        let cart = await this.cartService.isCartExist(userId);
+        cart ??= await this.cartService.createCart(userId);
+
+        cartItems.find( async (item: CartItemDTO) => {
+          if (typeof item.cart_item_id === 'string') {
+            await this.cartService.addToCart(cart.id, item.coffee_id, item.quantity);
+          }
+        });
+        await this.cartService.updateCartStatus(cart.id);
+  
+        this.channel.ack(msg);
       });
-      await this.cartService.updateCartStatus(cart.id);
- 
-      this.channel.ack(msg);
     });
   }
 
