a15446872f5a62190ffa0108de91ab16fbdc42eb
Joko Prabowo <jokoprabowo4550@gmail.com>
2026-02-16 20:10:08 +0700

    feat: apply signature verifier to webhook endpoint

--- services/api/src/controllers/payment.controller.ts
+++ services/api/src/controllers/payment.controller.ts
@@ -1,5 +1,8 @@
 import { NextFunction ,Request, Response } from 'express';
+import { verifyMidtransSignature } from '../utilities/midtrans';
 import { PaymentService } from '../services';
+import { ClientError } from '../exceptions';
+import config from '../config';
 
 class PaymentController {
   constructor(private readonly paymentService: PaymentService) {
@@ -8,7 +11,15 @@ class PaymentController {
 
   public async paymentWebhook(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      const { transaction_type, order_id } = req.body;
+      const { order_id, status_code, gross_amount, signature_key, transaction_type } = req.body;
+
+      const isValid = verifyMidtransSignature({
+        order_id, status_code, gross_amount, signature_key
+      }, config.SERVER_KEY!);
+
+      if (!isValid) {
+        throw new ClientError('Invalid signature key!');
+      }
       
       const transaction = await this.paymentService.createPaymentEvent(order_id, transaction_type, req.body);
       res.status(201).json({
