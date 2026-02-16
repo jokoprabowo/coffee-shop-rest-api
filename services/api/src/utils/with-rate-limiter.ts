7ee016b14b511a94cf38886e32c921eaa303934c
Joko Prabowo <jokoprabowo4550@gmail.com>
2026-02-16 20:49:58 +0700

    refactor: rename folder

--- services/api/src/utils/with-rate-limiter.ts
+++ services/api/src/utils/with-rate-limiter.ts
@@ -1,13 +1,6 @@
 import { RequestHandler } from 'express';
 import config from '../config';
 
-// export const withRateLimiter = (handler: () => RequestHandler): RequestHandler => (req, res, next) => {
-//   if (config.NODE_ENV === 'test') {
-//     return next();
-//   }
-//   return handler()(req, res, next);
-// };
-
 export const withRateLimiter = (limiter: RequestHandler): RequestHandler => (req, res, next) => {
   if (config.NODE_ENV === 'test') {
     return next();
