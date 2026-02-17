2611325303e48985405d6bfa45c6fd9429c5d8ea
Joko Prabowo <jokoprabowo4550@gmail.com>
2026-02-17 23:26:27 +0700

    feat: add update order status to order repo

--- services/consumer/src/repositories/order.repository.ts
+++ services/consumer/src/repositories/order.repository.ts
@@ -3,6 +3,16 @@ import { Pool } from 'pg';
 class OrderRepository {
   constructor(private readonly database: Pool) {}
 
+  public async updateOrderStatus(orderId: number, status: string): Promise<boolean> {
+    const query = {
+      text: 'update orders set status = $1, updated_at = $2 where id = $3',
+      values: [status, new Date(), orderId],
+    };
+
+    const { rowCount } = await this.database.query(query);
+    return (rowCount ?? 0) > 0;
+  }
+
   public async deleteUnpaidOrder(): Promise<boolean> {
     const query = {
       text: 'delete from orders where status in (\'unpaid\', \'cancelled\') and updated_at < now() - interval \'1 days\'',
