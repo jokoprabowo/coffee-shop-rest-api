98c2e40cfe3cf8d4f510e8832513cd1d2ce3a25d
Joko Prabowo <jokoprabowo4550@gmail.com>
2026-02-25 21:20:06 +0700

    test: create integration test for getMonthlyOrderStats endpoint

---
+++ services/api/__tests__/integration/orders/getMonthlyOrderStats.test.ts
@@ -0,0 +1,44 @@
+import request from 'supertest';
+import app from '../../../src/app';
+import { pool } from '@project/shared';
+import { generateAccessToken } from '../../../src/utils/token';
+
+describe('Get monthly order stats endpoint', () => {
+  let userId: number;
+  let adminId: number;
+  let userToken: string;
+  let adminToken: string;
+
+  beforeAll(async () => {
+    userId = await pool.query('select id from users where email = $1', ['testexample@mail.com'])
+      .then(res => res.rows[0].id);
+    userToken = generateAccessToken(userId);
+    adminId = await pool.query('select id from users where email = $1', ['adminexample@mail.com'])
+      .then(res => res.rows[0].id);
+    adminToken = generateAccessToken(adminId);
+  });
+
+  it('Should return a 200 status code and list of monthly order stats', async () => {
+    const response = await request(app).get('/api/v1/orders/stats?month=2&year=2026&statuses=paid')
+      .set('Authorization', `Bearer ${adminToken}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.data).toHaveProperty('orderStats');
+    expect(Array.isArray(response.body.data.orderStats)).toBe(true);
+  });
+  
+  it('Should return a 401 status code if access token is missing.', async () => {
+    const response = await request(app).get('/api/v1/orders/stats?month=2&year=2026&statuses=paid');
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.status).toBe('UNAUTHORIZED');
+  });
+
+  it('Should return a 403 status code if email registered is not an admin.', async () => {
+    const response = await request(app).get('/api/v1/orders/stats?month=2&year=2026&statuses=paid')
+      .set('Authorization', `Bearer ${userToken}`);
+
+    expect(response.statusCode).toBe(403);
+    expect(response.body.status).toBe('FORBIDDEN');
+  });
+});
