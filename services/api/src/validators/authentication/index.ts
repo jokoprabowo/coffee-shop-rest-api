992913e5132959c43ec85867f27e572ac3e9646f
Joko Prabowo <jokoprabowo4550@gmail.com>
2026-02-27 23:32:06 +0700

    refactor: implement reusable validation helper

--- services/api/src/validators/authentication/index.ts
+++ services/api/src/validators/authentication/index.ts
@@ -1,30 +1,14 @@
-import { ClientError } from '../../exceptions';
+import { validate } from '../validate';
 import { registerPayloadSchema, loginPayloadSchema } from './schema';
 import { UserDto, LoginUserDto } from '../../dto';
 
 const AuthenticateValidator = {
   validateRegisterPayload: (payload: UserDto) => {
-    const validationResult = registerPayloadSchema.validate(payload, {
-      abortEarly: false,
-      allowUnknown: true,
-    });
-
-    if (validationResult.error) {
-      const errorMessages = validationResult.error.details.map(detail => detail.message);
-      throw new ClientError(errorMessages.join(', '));
-    }
+    validate(registerPayloadSchema, payload);
   },
   
   validateLoginPayload: (payload: LoginUserDto) => {
-    const validationResult = loginPayloadSchema.validate(payload, {
-      abortEarly: false,
-      allowUnknown: true,
-    });
-
-    if (validationResult.error) {
-      const errorMessages = validationResult.error.details.map(detail => detail.message);
-      throw new ClientError(errorMessages.join(', '));
-    }
+    validate(loginPayloadSchema, payload);
   }
 };
 
--- services/api/src/validators/coffee/index.ts
+++ services/api/src/validators/coffee/index.ts
@@ -1,30 +1,14 @@
-import { ClientError } from '../../exceptions';
+import { validate } from '../validate';
 import { postCoffeeSchema, putCoffeeSchema } from './schema';
 import { CoffeeDto } from '../../dto';
 
 const CoffeeValidator = {
   validatePostCoffeePayload: (payload: CoffeeDto) => {
-    const validationResult = postCoffeeSchema.validate(payload, {
-      abortEarly: false,
-      allowUnknown: true,
-    });
-
-    if (validationResult.error) {
-      const errorMessages = validationResult.error.details.map(detail => detail.message);
-      throw new ClientError(errorMessages.join(', '));
-    }
+    validate(postCoffeeSchema, payload);
   },
 
   validatePutCoffeePayload: (payload: CoffeeDto) => {
-    const validationResult = putCoffeeSchema.validate(payload, {
-      abortEarly: false,
-      allowUnknown: true,
-    });
-
-    if (validationResult.error) {
-      const errorMessages = validationResult.error.details.map(detail => detail.message);
-      throw new ClientError(errorMessages.join(', '));
-    }
+    validate(putCoffeeSchema, payload);
   }
 };
 
